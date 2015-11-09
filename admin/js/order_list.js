8454e01c57935be72f9cd54d8eac83eb7b54551f
Artem Leontiev <aleontiev@hamstersoft.com>
2015-11-10 01:26:17 +0300

    Add sweetalert in project and small bugfix. Add alert reason list in page orders for distrib

--- admin/js/order_list.js
+++ admin/js/order_list.js
@@ -1,8 +1,31 @@
 requirejs(['./common'], function (common) {
-    require(['jquery', 'bootstrap'], function ($) {
-        'use strict';
+    require(['jquery', 'bootstrap', 'sweetalert'], function ($) {
+        $("head").append($("<link rel='stylesheet' href='/css/lib/sweetalert.css'>"));
+        $("head").append($("<link rel='stylesheet' href='/css/lib/sweetalert_tpl.css'>"));
+        $('button.returnSelect').click(function(e) {
+            swal({
+                title: "Why are you returning this?",
+                html: true,
+                text: "<p>Sub additional text</p>" +
+                "<select class='form-control'>" +
+                "<option>The item is spoilt/defective</option>" +
+                "<option>The wrong item was delivered</option>" +
+                "<option>Arrived in addition to what was ordered</option>" +
+                "<option>Missed estimated delivery date</option>" +
+                "<option>Ordered the wrong SKU</option>" +
+                "</select>",
+                type: "info",
+                showCancelButton: true,
+                confirmButtonClass: "btn-success",
+                closeOnConfirm: false
+            }, function(selected) {
+                if (selected != "") {
+                    window.location.href = '/admin/views/return.html';
+                }
+            });
 
-
+            return false;
+        });
     });
 });
 
